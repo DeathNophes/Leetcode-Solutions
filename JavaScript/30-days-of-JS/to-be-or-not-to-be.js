aae7fb249d9e68a03facd162e0f769babb7870e9
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2025-02-27 15:35:34 +0200

    add new solutions

---
+++ JavaScript/30-days-of-JS/to-be-or-not-to-be.js
@@ -0,0 +1,17 @@
+function expect (val) {
+
+    function toBe (n) {
+        if (val === n) return true
+        else throw Error("Not Equal")
+    }
+    
+    function notToBe (n) {
+        if (val !== n) return true
+        else throw Error("Equal")
+    }
+
+    return {toBe, notToBe}
+}
+
+// expect(5).toBe(5)
+// expect(5).toBe(null)
