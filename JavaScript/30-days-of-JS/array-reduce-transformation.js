b51881071777c11868ceac9de2e9f71e70b63322
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2025-02-28 16:02:17 +0200

    add solution

---
+++ JavaScript/30-days-of-JS/array-reduce-transformation.js
@@ -0,0 +1,17 @@
+// let nums = [1, 2, 3, 4];
+// let fn = function sum (accum, curr) {return accum + curr};
+// const init = 0
+
+function reduce (nums, fn, init) {
+    if (nums.length === 0) return init
+
+    let value = init;
+
+    for (let i = 0; i < nums.length; i++) {
+        value = fn(value, nums[i])
+    }
+
+    return value
+}
+
+// reduce(nums, fn, init)
