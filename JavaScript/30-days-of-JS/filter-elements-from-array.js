dca93117a1c0ad14ee8a339528e3bb66648eac38
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2025-02-28 15:43:56 +0200

    add solution

---
+++ JavaScript/30-days-of-JS/filter-elements-from-array.js
@@ -0,0 +1,17 @@
+let arr = [-2, -1, 0, 1, 2];
+let fn = function plusOne (n) {return n + 1}
+
+function filter (arr, fn) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) newArr.push(arr[i])
+    }
+
+    // JS treat nonzero numbers as truthy, and 0 (zero) as falsy
+
+    console.log(newArr);
+    return newArr;
+}
+
+filter(arr, fn);
