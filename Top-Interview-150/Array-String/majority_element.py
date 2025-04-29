8ce06a0860440ae58672636e0f0a94d47411e5eb
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2025-04-29 11:24:20 +0300

    add changes to majority_element.py

--- Top-Interview-150/Array-String/majority_element.py
+++ Top-Interview-150/Array-String/majority_element.py
@@ -1,3 +1,10 @@
+# The idea here is that we "squash" all elements that aren't the majority element, one by one.
+# In the end, only the majority element remains.
+
+# This is the Boyer-Moore Majority Vote algorithm. The idea is that whenever we come across an element
+# that is NOT the candidate for the majority element, they "neutralize" each other.
+# If there is a true majority element, it will survive the process in the end.
+
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
         count = 0
